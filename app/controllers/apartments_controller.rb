989306b222afa07bac8a26e4c67fef05c12b4587
learn <>
2017-11-14 16:05:53 -0800

    added images

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -69,6 +69,6 @@ class ApartmentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def apartment_params
-      params.require(:apartment).permit(:street1, :street2, :city, :postal_code, :state, :country, :manager, :phone, :contact_hours)
+      params.require(:apartment).permit(:street1, :street2, :city, :postal_code, :state, :country, :manager, :phone, :contact_hours, :image)
     end
 end

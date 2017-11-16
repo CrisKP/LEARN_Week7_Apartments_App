8f486f7010601274b51828bbaeadfa161d4683a1
learn <>
2017-11-16 14:21:52 -0800

    added admin panel

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get "admin" => "admin#index"
+  put "admin/:id" => "admin#update"
+  patch "admin/:id" => "admin#update"
+
+  get 'admin/update'
+
   resources :apartments
   root 'apartments#index'
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
